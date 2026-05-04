f61c58d0166a4fb9269ad4bff0f8d56654c9e655
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2026-05-04 16:33:03 -0300

    fix: add type adapterType

--- lib/types.ts
+++ lib/types.ts
@@ -14,6 +14,7 @@ export interface VpsServer {
   subdomain: string
   ip: string
   providerUrl: string
+  providerApiKey?: string | null
   adapterType: string
   managerType?: string | null
   managerUrl?: string | null
